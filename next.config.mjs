ecc1de75e0a9f4e11909f21f7fa83e47bc667055
pawoot3129 <pwmu3129@gmail.com>
2026-06-22 10:05:15 +0700

    fix config mjs style

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // สั่งให้ข้ามการเช็ก ESLint ตอน Build บน Vercel
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // สั่งให้ข้ามการเช็กประเภท TypeScript ตอน Build
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
