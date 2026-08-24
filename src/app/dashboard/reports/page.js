f4b3610d9d883576c3b56384f340a644b931c61e
pawoot3129 <pwmu3129@gmail.com>
2026-08-24 15:00:04 +0700

    update

--- src/app/dashboard/reports/page.js
+++ src/app/dashboard/reports/page.js
@@ -11,18 +11,19 @@ const reportOptions = [
         gradient: "from-blue-500 to-indigo-600"
     },
     {
-        title: "รายงานสรุปผลรายกิจกรรม",
-        description: "ติดตามผลการเข้าร่วมและประเมินผลของแต่ละกิจกรรมที่จัดขึ้น",
-        path: "/dashboard/reports/activity-summary",
-        status: "พร้อมใช้งาน",
-        gradient: "from-emerald-500 to-teal-600"
-    },
-    {
         title: "รายงานสรุปผลสถิติการมาเรียนประจำเดือน",
         description: "สรุปผลสถิติการเข้าร่วมกิจกรรมและการมาเรียนรายเดือนของนักศึกษา",
         path: "/dashboard/reports/monthly-summary",
         status: "พร้อมใช้งาน",
         gradient: "from-blue-600 to-purple-600"
+                        
+    },
+    {
+        title: "รายงานสรุปผลรายกิจกรรม",
+        description: "ติดตามผลการเข้าร่วมและประเมินผลของแต่ละกิจกรรมที่จัดขึ้น",
+        path: "/dashboard/reports/activity-summary",
+        status: "พร้อมใช้งาน",
+        gradient: "from-emerald-500 to-teal-600"
     },
     {
         title: "รายงานสรุปผลกิจกรรมปลายภาคเรียน",
