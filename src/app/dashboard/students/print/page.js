c2683c0473ef65c7f02f2ae6d79dd2eb48db3614
pawoot3129 <pwmu3129@gmail.com>
2026-08-27 11:35:55 +0700

    update

--- src/app/dashboard/students/print/page.js
+++ src/app/dashboard/students/print/page.js
@@ -106,7 +106,7 @@ export default function PrintStudentsPage() {
                         padding-bottom: 5mm !important;
                     }
                     .print-page:last-child { page-break-after: auto; break-after: auto; }
-                    table th, table td { padding: 4px 6px !important; font-size: 12.5px !important; }
+                    table th, table td { padding: 4px 6px !important; font-size: 14px !important; }
                 }
             `}</style>
             
